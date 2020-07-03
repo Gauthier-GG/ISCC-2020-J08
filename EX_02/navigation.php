33584da0b787c2c5b69bae6d8fdd205e31b9fc96
Gauthier-GG <67266035+Gauthier-GG@users.noreply.github.com>
2020-07-03 17:03:43 +0200

    PHP / HTML / cerveau en sueur
    heu

--- EX_02/navigation.php
+++ EX_02/navigation.php
@@ -2,7 +2,6 @@
  <li><a href="http://localhost:8888/ISCC-2020/ISCC-2020-J08/EX_02/?page=accueil">Accueil</a></li>
  <li><a href="http://localhost:8888/ISCC-2020/ISCC-2020-J08/EX_02/?page=programme">Programme</a></li>
  <li><a href="http://localhost:8888/ISCC-2020/ISCC-2020-J08/EX_02/?page=contacts">Contacts</a></li>
- <li><a href="http://localhost:8888/ISCC-2020/ISCC-2020-J08/EX_02/?page=contact-form">Contact-form</a></li>
 </nav>
 
 
